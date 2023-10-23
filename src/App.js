83dfbe7860d23d0a6e947e4167133ec0e1ee1178
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2023-10-23 14:33:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { sculptureList } from "./data.js";
 
-function App() {
+// export default function App() {
+//   const [index, setIndex] = useState(0);
+//   const [showMore, setShowMore] = useState(false);
+//   const hasNext = index < sculptureList.length - 1;
+
+//   function handleNextClick() {
+//     if (hasNext) {
+//       setIndex(index + 1);
+//     } else {
+//       setIndex(0);
+//     }
+//   }
+
+//   function handleMoreClick() {
+//     setShowMore(!showMore);
+//   }
+
+//   let sculpture = sculptureList[index];
+//   return (
+//     <>
+//     <button onClick={handleNextClick}>
+//       Next
+//     </button>
+//     <h2>
+//       <i>{sculpture.name}</i>
+//       by {sculpture.artist}
+//     </h2>
+//     <h3>
+//       ({index + 1} of {sculptureList.length})
+//     </h3>
+//     <button onClick={handleMoreClick}>
+//       {showMore ? 'Hide' : 'Show'} details
+//     </button>
+//     {showMore && <p>{sculpture.description}</p>}
+//     <img
+//       src={sculpture.url}
+//       alt={sculpture.alt}
+//     />
+//     </>
+//   );
+// }
+
+export default function App() {
+  const [person, setPerson] = useState({
+    name: 'Niki de Saint Phalle',
+    artwork: {
+      title: 'Blue Nana',
+      city: 'Hamburg',
+      image: 'https://i.imgur.com/Sd1AgUOm.jpg',
+    }
+  });
+
+  function handleNameChange(e) {
+    setPerson({
+      ...person,
+      name: e.target.value
+    });
+  }
+
+  function handleTitleChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        title: e.target.value
+      }
+    });
+  }
+
+  function handleCityChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        city: e.target.value
+      }
+    });
+  }
+
+  function handleImageChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        image: e.target.value
+      }
+    });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <label>
+        Name:
+        <input
+          value={person.name}
+          onChange={handleNameChange}
+        />
+      </label>
+      <label>
+        Title:
+        <input
+          value={person.artwork.title}
+          onChange={handleTitleChange}
+        />
+      </label>
+      <label>
+        City:
+        <input
+          value={person.artwork.city}
+          onChange={handleCityChange}
+        />
+      </label>
+      <label>
+        Image:
+        <input
+          value={person.artwork.image}
+          onChange={handleImageChange}
+        />
+      </label>
+      <p>
+        <i>{person.artwork.title}</i>
+        {' by '}
+        {person.name}
+        <br />
+        (located in {person.artwork.city})
+      </p>
+      <img
+        src={person.artwork.image}
+        alt={person.artwork.title}
+      />
+    </>
   );
 }
-
-export default App;
